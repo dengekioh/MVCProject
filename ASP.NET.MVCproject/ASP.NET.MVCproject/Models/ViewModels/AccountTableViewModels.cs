5d0668c0874476b7b567e044b8d5377d58ada434
Charles Huang <huangcharles@micron.com>
2019-07-12 15:52:59 +0800

    push 2nd

---
+++ ASP.NET.MVCproject/ASP.NET.MVCproject/Models/ViewModels/AccountTableViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models.ViewModels
+{
+    public class AccountTableViewModels
+    {
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Remark { get; set; }
+    }
+}
